cc6ec6760283063963c70560db75f01eb6e25e55
Bruno Noveli <brunimzuppwolf@gmail.com>
2024-01-26 14:20:22 -0300

    Added fragments as pages to each icon in BottomNavigation

--- app/src/main/java/com/novelitech/practicebottomnavigationkotlin/MainActivity.kt
+++ app/src/main/java/com/novelitech/practicebottomnavigationkotlin/MainActivity.kt
@@ -2,6 +2,7 @@ package com.novelitech.practicebottomnavigationkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.novelitech.practicebottomnavigationkotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,5 +15,27 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
+
+        val resume = ResumeFragment()
+        val settings = SettingsFragment()
+        val profile = ProfileFragment()
+
+        setCurrentFragment(resume)
+
+        binding.bottomNavigation.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.miResume -> setCurrentFragment(resume)
+                R.id.miSettings -> setCurrentFragment(settings)
+                R.id.miProfile -> setCurrentFragment(profile)
+            }
+            true
+        }
+    }
+
+    private fun setCurrentFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.pages, fragment)
+            commit()
+        }
     }
 }
